589a6eabb848b771a52d0d37203f6380dda2ccfe
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-06-02 19:07:47 +0330

    some modifications

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ const container = document.querySelector(".container");
 
 if (localStorage.getItem("container")) {
   todos = localStorage.getItem("container");
-  console.log(todos);
   container.innerHTML = todos;
-  // localStorage.clear();
+  localStorage.clear();
 }
 
 const input = document.querySelector("input");
@@ -12,17 +11,17 @@ const input = document.querySelector("input");
 input.addEventListener("keydown", (e) => {
   console.log(e.key);
   if (e.key == "Enter") {
-    createTodo();
+    createTodo(input.value);
     input.value = "";
   }
 });
 
-function createTodo() {
+function createTodo(text) {
   const todo = document.createElement("div");
   todo.classList.add("todo");
   todo.setAttribute("onClick", "leftClick(this)");
   todo.setAttribute("oncontextmenu", "rightClick(this)");
-  todo.textContent = input.value;
+  todo.textContent = text;
   container.appendChild(todo);
   window.localStorage.setItem("container", container.innerHTML);
 }
