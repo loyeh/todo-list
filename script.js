867621e6f197dd21b0c60944d9b4b5e3e8858850
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-06-02 18:49:27 +0330

    add localstorage functionality

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const container = document.querySelector(".container");
+
+if (localStorage.getItem("container")) {
+  todos = localStorage.getItem("container");
+  console.log(todos);
+  container.innerHTML = todos;
+  // localStorage.clear();
+}
+
 const input = document.querySelector("input");
 
 input.addEventListener("keydown", (e) => {
@@ -12,6 +20,20 @@ input.addEventListener("keydown", (e) => {
 function createTodo() {
   const todo = document.createElement("div");
   todo.classList.add("todo");
+  todo.setAttribute("onClick", "leftClick(this)");
+  todo.setAttribute("oncontextmenu", "rightClick(this)");
   todo.textContent = input.value;
   container.appendChild(todo);
+  window.localStorage.setItem("container", container.innerHTML);
+}
+
+function leftClick(elem) {
+  elem.classList.toggle("corrected");
+  window.localStorage.setItem("container", container.innerHTML);
+}
+
+function rightClick(elem) {
+  event.preventDefault();
+  elem.remove();
+  window.localStorage.setItem("container", container.innerHTML);
 }
